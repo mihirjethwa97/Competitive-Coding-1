2bf26f9938d494915660a3a77660574e15cdd9de
mihirjethwa97 <mihirjethwa97@gmail.com>
2024-06-28 19:43:55 -0400

    Comp Problem 1 solved

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,22 @@
 
+public class Main
+{
+    static public int binarySearch(int[] nums){
+        int left =0;
+        int right = nums.length-1;
+        while(left < right){
+            int mid = left + (right-left)/2;
+            if(nums[left]==left+1 && nums[mid] == mid+1){
+                left = mid + 1;
+            }else{
+                right = mid;
+            }
+        }
+        return nums[left] - 1;
+    }
+    public static void main(String[] args) {
+        int [] nums = new int[]{3};
+        int res = binarySearch(nums);
+        System.out.println(res);
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Competitive-Coding-1
 
+Problem 1 solved
+
 Please submit the interview problems posted in slack channel here. The problems and statements are intentionally not shown here so that students are not able to see them in advance 
